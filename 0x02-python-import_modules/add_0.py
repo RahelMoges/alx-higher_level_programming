1f5e76b925704ee3a708a72c8191f6118ddb3fb3
RahelMoges <rahelmoges192@gmail.com>
2022-12-01 08:44:38 -0800

    haha

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def add(a,b):
-"""my addition function
+def add(a, b):
+"""My addition function
 Args:
-a:first integer
-b:second integer
+a: first integer
+b: second integer
 Returns:
-the return value. a+b
+The return value. a + b
 """
-return (a+b)
+return (a + b)
