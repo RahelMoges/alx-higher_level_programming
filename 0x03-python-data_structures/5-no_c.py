b8a16fc1782cb1ebf165906e445c8ae0486d8b5d
RahelMoges <rahelmoges192@gmail.com>
2022-12-06 06:24:19 -0800

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,7 @@
-
-_c(my_string):
-
+#!/usr/bin/python3
+def no_c(my_string):
         listofchars = list(my_string)
-
             for char in listofchars:
-
-                        if char == 'c' or char == 'C':
-
+                 if char == 'c' or char == 'C':
                                         listofchars.remove(char)
-
-                                            return("".join(listofchars))
+                                         return("".join(listofchars))
