c997c2616a1a25eb94b725b8ce5fba2772bd57bd
RahelMoges <rahelmoges192@gmail.com>
2022-12-19 14:59:45 -0800

    py

--- 0x05-python-exceptions/3-main.py
+++ 0x05-python-exceptions/3-main.py
@@ -3,7 +3,6 @@
 safe_print_division = __import__('3-safe_print_division').safe_print_division
 
 
-
 a = 12
 
 b = 2
@@ -11,7 +10,6 @@ b = 2
 result = safe_print_division(a, b)
 
 print("{:d} / {:d} = {}".format(a, b, result))
-
 
 
 a = 12
