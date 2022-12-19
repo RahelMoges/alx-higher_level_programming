8cbdb2ab559c246c02494d405c40964d3b752f38
RahelMoges <rahelmoges192@gmail.com>
2022-12-19 12:16:16 -0800

    exeption

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#1/usr/bin/python3
+def safe_print_list(my_list=[], x=0_)
+i = 0
+printed = 0
+for i in range(0,x):
+try:
+print("{}".format(my_list[i]), end="")
+printed =+ 1
+except:
+continue
+  print()
+return printed
