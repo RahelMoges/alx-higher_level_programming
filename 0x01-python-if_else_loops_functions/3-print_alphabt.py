38aab3ade9fe0ef49bdb3419bf71fdff37c63c1d
RahelMoges <rahelmoges192@gmail.com>
2023-01-09 04:13:53 -0800

    if

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#/usr/bin/python3
-#Author - Tolulope Fakunle
-for letter in range(97, 123):
-    if char(lettr) != 'q' and chr(letter) != 'e':
-        print("{}'.format(chr(letter)), end="")
+#!/usr/bin/python3
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
