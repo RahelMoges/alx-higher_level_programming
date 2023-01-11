a9acf57381164bca0f135843b4a543c39abe5cf8
RahelMoges <rahelmoges192@gmail.com>
2023-01-10 16:03:50 -0800

    to JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines a string-to-JSON """
+
+import json
+
+
+def to_json_string(my_obj):
+
+    """Return the JSON representation of a string object."""
+
+    return json.dumps(my_obj)
