ba901e765209ee7d6dbee16a0787530b142b6183
RahelMoges <rahelmoges192@gmail.com>
2023-01-15 16:21:26 -0800

    square with me

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+
 """Define a class Square"""
+
+
 class Square:
+
     """Square with size"""
+
     def __init__(self, size):
+
         """Initialize a new Square"""
+
         self.__size = size
